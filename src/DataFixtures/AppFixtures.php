f2f7fafa63f974b4456e7f6b94f3d4c0d625b4df
Oussama Bel <oussamabelfarhi@gmail.com>
2024-02-16 12:01:02 +0100

    Création des fixtures de l'entity User et Client

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,13 +2,19 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Client;
 use App\Entity\Mobile;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(private readonly UserPasswordHasherInterface $passwordHasher) {}
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
@@ -29,10 +35,30 @@ class AppFixtures extends Fixture
                 ->setRamSize($faker->numberBetween(4, 16));
 
             $manager->persist($mobile);
-
         }
 
-        $manager->persist($mobile);
+        $client = new Client();
+        $client->setEmail('client@example.com');
+        $client->setRoles(['ROLE_USER']);
+        $client->setPassword($this->passwordHasher->hashPassword($client,'password123'));
+        $client->setFirstName('Emmanuel');
+        $client->setLastName('Doen');
+        $client->setAddress('123 rue Jean Jaures 75016 Paris');
+        $client->setSlug('emmanuel-doen');
+        $client->setSiret(123456789);
+        $client->setPhone(33745267826);
+        $client->setCreatedAt(new \DateTimeImmutable());
+
+        for($i=1;$i<20;$i++) {
+            $user = new User();
+            $user->setFirstName($faker->firstName);
+            $user->setLastName($faker->lastName);
+            $user->setEmail($faker->unique()->email);
+            $user->setPassword($this->passwordHasher($user, 'password123'));
+            $user->setClient($client);
+        }
+
+        $manager->persist($client);
 
         $manager->flush();
     }
