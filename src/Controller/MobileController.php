2cf3eeee7b829c1236cb8699acb1969c4aee0b4a
Oussama Bel <oussamabelfarhi@gmail.com>
2024-02-16 11:08:55 +0100

    Creation de l'entity Client

--- src/Controller/MobileController.php
+++ src/Controller/MobileController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Mobile;
+use App\Repository\MobileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class MobileController extends AbstractController
 {
-    #[Route('/api/mobiles', name: 'mobile')]
-    public function getAllMobiles(): JsonResponse
+    #[Route('/api/mobiles', name: 'mobiles', methods: ['GET'])]
+    public function getAllMobiles(MobileRepository $mobileRepository,
+                                  SerializerInterface $serializer): JsonResponse
     {
-        return new JsonResponse([
+        $mobileList = $mobileRepository->findAll();
 
-        ]);
+        $jsonMobileList = $serializer->serialize($mobileList, 'json');
+        return new JsonResponse($jsonMobileList, Response::HTTP_OK, [],true);
+    }
+
+    #[Route('/api/mobiles/{id}', name: 'detailMobile', methods: ['GET'])]
+    public function getDetailMobile(Mobile $mobile, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonMobile = $serializer->serialize($mobile, 'json');
+        return new JsonResponse($jsonMobile, Response::HTTP_OK, [],true);
     }
 }
